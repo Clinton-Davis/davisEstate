3ed3a275fe5025be304e4c6273c6fcd42ccc4b78
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-03-03 21:04:14 +0000

    added login to about page and list-detail page

--- pages/views.py
+++ pages/views.py
@@ -11,10 +11,10 @@ def index(request):
     return render(request, 'pages/index.HTML', context)    
 
 def about(request):
-    agent = Agent.objects.order_by('-hire-date')
+    agent = Agent.objects.order_by('-hire_date')
     mvp_agent = Agent.objects.all().filter(is_mvp=True)
     context = {
         'agents': agent,
-        'mvp_agents': mvp_agent
+        'mvp_agent': mvp_agent
     }
     return render(request, 'pages/about.HTML', context)    
