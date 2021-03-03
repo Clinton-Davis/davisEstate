9b31c8313e49b1eddc646db44b60c18b45478e0d
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-03-03 16:24:20 +0000

    added login to index(home-page) added login to about but not html template

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Listing
 
 def listings(request):
-     return render(request, 'listings/listings.html', {})
+     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+     paginator = Paginator(listings, 6)
+     page = request.GET.get('page')
+     page_listings = paginator.get_page(page)
+     context = {
+          'listings' : page_listings,
+     }
+     return render(request, 'listings/listings.html', context)
     
-def listdetails(request):
-     return render(request, 'listings/list_details.html', {})
+def list_details(request, listing_id):
+     return render(request, 'listings/list_detail.html', {})
     
 def search(request):
      return render(request, 'listings/search.html', {})
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from agents.models import Agent
 
 def index(request):
-    context = {}
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+    context = {
+        'listings': listings
+    }
     return render(request, 'pages/index.HTML', context)    
 
 def about(request):
-    context = {}
+    agent = Agent.objects.order_by('-hire-date')
+    mvp_agent = Agent.objects.all().filter(is_mvp=True)
+    context = {
+        'agents': agent,
+        'mvp_agents': mvp_agent
+    }
     return render(request, 'pages/about.HTML', context)    
