95215fa8d81eabc2f727d80e9dd5523b8467bd73
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-03-04 16:36:10 +0000

    added contect app, added models, have emails being sent to backend but not smpt

--- destate/urls.py
+++ destate/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', include('pages.urls', namespace='pages')),
     path('listings/', include('listings.urls', namespace='listings')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('contact/', include('contact.urls', namespace='contacts')),
     path('admin/', admin.site.urls),
 ] 
 if settings.DEBUG:
