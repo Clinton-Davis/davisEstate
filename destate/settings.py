69b8d708abe8fbf4253ed10e7668243778b85245
Clitnon-Davis <clintongadavis@gmail.com>
2022-04-10 11:43:45 +0100

    changed locatation of static folder

--- destate/settings.py
+++ destate/settings.py
@@ -23,12 +23,13 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['davis-estates.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -138,9 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "destate/static"),)
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = "static_root"
 
 # WHITENOISE - static storage
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
---
+++ static_root/js/googlemaps.js
@@ -0,0 +1,27 @@
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 16,
+    center: { lat: 53.35014, lng: -6.266155 },
+    streetViewControl: false,
+    mapTypeId: "hybrid",
+    mapTypeControl: false,
+  });
+  const geocoder = new google.maps.Geocoder();
+  geocodeAddress(geocoder, map);
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  const address = document.getElementById("address").value;
+  geocoder.geocode({ address: address }, (results, status) => {
+    if (status === "OK") {
+      resultsMap.setCenter(results[0].geometry.location);
+      new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location,
+      });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+initMap();
